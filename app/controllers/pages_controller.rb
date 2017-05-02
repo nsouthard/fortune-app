2ed864ed31075e5411d6996fcd46d2db8753ae9b
nsouthard <nathan.southard@gmail.com>
2017-05-01 22:06:50 -0500

    End of night save 5/1/2017

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,26 @@
 class PagesController < ApplicationController
   def fortune
     @title = 'What is your fortune?'
-    render 'fortune_teller.html'
+    list_of_fortunes = ["You will be rich and very successful.", 
+              "You will be poor but happy in a weird way.", 
+               "I'm having trouble seeing it clearly... Do you have another $20?"]
+    fortune_index = rand(0..list_of_fortunes.length - 1)
+    @fortune = list_of_fortunes[fortune_index]
+    render 'fortune_teller.html.erb'
   end
 
   def numgenerator
-    @title = 'Lotto numbers'
-    6.times do |number| 
-      number = rand(61)
-      puts number
+    list_of_numbers = []
+    6.times do 
+      list_of_numbers << rand(1..60)
     end
-    render 'lotto_numbers.html'
+    @winning_numbers = list_of_numbers.join(", ")
+    render 'lotto_numbers.html.erb'
   end
+
+  def counter
+    @page_count = 1
+    render 'visits.html.erb'
+  end
+
 end
